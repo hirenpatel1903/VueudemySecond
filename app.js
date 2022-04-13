b564a587c4f9fd1cd7097569095ef96d3a538273
Hiren Patel <patelhiren.hp19@gmail.com>
2022-04-13 09:59:49 +0530

    bind method

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'finish the course and learn Vue!'
+            courseGoal: 'finish the course and learn Vue!',
+            vueLink: 'https://vuejs.org/guide/introduction.html'
         };
     }
 });
